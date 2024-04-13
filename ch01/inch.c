452f82bfe389066ebdb6a40f1a60718cf27b3b59
Daniel <nhatrangvtd@gmail.com>
2024-04-13 22:22:20 +0700

    new inch

---
+++ ch01/inch.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int num;
+  num = 1;
+  printf("I am a simple");
+  printf(" computer.\n");
+  printf("My favourit number is %d.\n", num);
+  return 0;
+}
